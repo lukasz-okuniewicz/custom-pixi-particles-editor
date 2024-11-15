dabe7f9c9b2b8f86c00d2492ffa84f310bb392c7
lukasz-okuniewicz <33227137+lukasz-okuniewicz@users.noreply.github.com>
2024-11-15 22:05:37 +0100

    Add files via upload

--- components/menu/properties/CollisionProperties.d.ts
+++ components/menu/properties/CollisionProperties.d.ts
@@ -4,23 +4,33 @@ export interface IProps {
     updateProps: any;
     activeEffect: any;
     app: any;
+    helpingLines: boolean;
 }
-declare class CollisionProperties extends React.Component<IProps> {
-    state: {
-        isSubmenuVisible: string;
-        selectedPointIndex: null;
-    };
+interface IState {
+    isSubmenuVisible: string;
+    pointKey: string;
+    selectedLineIndex: number | null;
+}
+declare class CollisionProperties extends React.Component<IProps, IState> {
+    state: IState;
     componentDidMount(): void;
     componentWillUnmount(): void;
     render(): React.JSX.Element;
     private handleChangeEnabled;
+    private updateLinePoint;
     private updateProps;
-    private updatePoints;
-    private addPoint;
-    private removePoint;
-    private changeSubmenuVisibility;
     private selectPoint;
+    private addLine;
+    private removeLine;
+    private changeSubmenuVisibility;
     private handleWindowClick;
+    private handleChangeLines;
+    private skipPositionBehaviourOnCollision;
+    private skipAngularVelocityBehaviourOnCollision;
+    private skipColorBehaviourOnCollision;
+    private skipEmitDirectionBehaviourOnCollision;
+    private skipRotationBehaviourOnCollision;
+    private skipSizeBehaviourOnCollision;
 }
 declare const _default: typeof CollisionProperties;
 export default _default;
